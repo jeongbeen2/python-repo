e93709a77f597516039e2732c1fa00401891fc17
jeongbeen2 <jb2n95@kakao.com>
2020-09-18 17:02:03 +0900

    linear & binary search algo

--- codeit/codeit_algo01.py
+++ codeit/codeit_algo01.py
@@ -1,19 +1,13 @@
-def binary_search(element, some_list):
-    start_index = 0
-    end_index = len(some_list) - 1
-    
-    while start_index <= end_index:
-        half_index = (start_index+end_index)//2
-        if element == some_list[half_index]:
-            return half_index
-        elif element < some_list[half_index]:
-            end_index = half_index - 1
-        else:
-            start_index = half_index + 1
+
+def linear_search(element, some_list):
+    for i in some_list:
+        if element == i:
+            return some_list.index(i)
     return None
 
-print(binary_search(2, [2, 3, 5, 7, 11]))
-print(binary_search(0, [2, 3, 5, 7, 11]))
-print(binary_search(5, [2, 3, 5, 7, 11]))
-print(binary_search(3, [2, 3, 5, 7, 11]))
-print(binary_search(11, [2, 3, 5, 7, 11]))
+
+print(linear_search(2, [2, 3, 5, 7, 11]))
+print(linear_search(0, [2, 3, 5, 7, 11]))
+print(linear_search(5, [2, 3, 5, 7, 11]))
+print(linear_search(3, [2, 3, 5, 7, 11]))
+print(linear_search(11, [2, 3, 5, 7, 11]))
