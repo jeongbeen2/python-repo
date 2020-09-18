619aa237879ad7b9986c8c1a69fcc69a73d7d386
jeongbeen2 <jb2n95@kakao.com>
2020-09-18 18:09:13 +0900

    wip

--- codeit/codeit_algo02.py
+++ codeit/codeit_algo02.py
@@ -1,7 +1,7 @@
 def binary_search(element, some_list):
     start_index = 0
     end_index = len(some_list) - 1
-    
+
     while start_index <= end_index:
         half_index = (start_index+end_index)//2
         if element == some_list[half_index]:
@@ -11,6 +11,7 @@ def binary_search(element, some_list):
         else:
             start_index = half_index + 1
     return None
+
 
 print(binary_search(2, [2, 3, 5, 7, 11]))
 print(binary_search(0, [2, 3, 5, 7, 11]))
---
+++ codeit/codeit_algo03.py
@@ -0,0 +1,6 @@
+num_list = [3, 6, 1, 2, 3, 8, 5]
+
+
+for i in range(len(num_list)):
+    for j in range(len(num_list)):
+        if num_list[i] > num_list[j]:
