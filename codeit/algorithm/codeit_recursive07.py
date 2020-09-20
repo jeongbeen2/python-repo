33ee44d3062570eee15b8758744a41e75853da43
jeongbeen2 <jb2n95@kakao.com>
2020-09-20 18:03:02 +0900

    recursive binary search

--- codeit/algorithm/codeit_recursive07.py
+++ codeit/algorithm/codeit_recursive07.py
@@ -1,10 +1,21 @@
+
 def binary_search(element, some_list, start_index=0, end_index=None):
     # end_index가 따로 주어지지 않은 경우에는 리스트의 마지막 인덱스
     if end_index == None:
         end_index = len(some_list) - 1
 
-    if start_index:
-        pass
+    if start_index > end_index:
+        return None
+    else:
+        mid_idx = (start_index+end_index)//2
+        if element == some_list[mid_idx]:
+            return mid_idx
+        elif element < some_list[mid_idx]:
+            end_index = mid_idx - 1
+            return binary_search(element, some_list, start_index, end_index)
+        else:
+            start_index = mid_idx + 1
+            return binary_search(element, some_list, start_index, end_index)
 
 
 print(binary_search(2, [2, 3, 5, 7, 11]))
