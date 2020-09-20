31722b516b04de64617622ab39b6bf6768e82feb
jeongbeen2 <68345069+jeongbeen2@users.noreply.github.com>
2020-09-20 14:26:04 +0900

    recursive binary search

--- codeit/algorithm/codeit_algo01.py
+++ codeit/algorithm/codeit_algo01.py
@@ -1,5 +1,5 @@
-
 # Linear Search Algorism
+
 
 def linear_search(element, some_list):
     for i in some_list:
@@ -13,4 +13,3 @@ print(linear_search(0, [2, 3, 5, 7, 11]))
 print(linear_search(5, [2, 3, 5, 7, 11]))
 print(linear_search(3, [2, 3, 5, 7, 11]))
 print(linear_search(11, [2, 3, 5, 7, 11]))
-d
