05c24860d26a1b8a6aff6d1fd37cc575eadf91cf
jeongbeen2 <jb2n95@kakao.com>
2020-09-24 15:27:35 +0900

    d&c #2

--- codeit/algorithm/codeit_d&c02.py
+++ codeit/algorithm/codeit_d&c02.py
@@ -1,7 +1,19 @@
 def merge(list1, list2):
     merge_list = []
-
-    for i in range
+    i = 0
+    j = 0
+    while i < len(list1) and j < len(list2):
+        if list1[i] > list2[j]:
+            merge_list.append(list2[j])
+            j += 1
+        else:
+            merge_list.append(list1[i])
+            i += 1
+    if i == len(list1):
+        merge_list += list2[j:]
+    elif j == len(list2):
+        merge_list += list1[i:]
+    return merge_list
 
 
 # 테스트
